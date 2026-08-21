1a8812481597d70d7961bfadec47f1d643fd4594
ShreyasKR8 <skrravi08@gmail.com>
2026-08-21 17:22:37 +0000

    feat: add initial root route for server startup

---
+++ app.js
@@ -0,0 +1,21 @@
+import express from "express";
+import { fileURLToPath } from "node:url";
+import path from "node:path";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+    res.send("FileDock is running");
+});
+
+app.listen(PORT, () => {
+    console.log(`server listening at ${PORT}`);
+});
